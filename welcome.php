2ab9b31edb8a395edb591b74b09ecbe904141be9
tarturr <tartur.dev@gmail.com>
2023-11-11 18:27:12 +0100

    Added header and some style to the welcome.php page

--- welcome.php
+++ welcome.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenue !</title>
+    <title>DiscordIn - Bienvenue !</title>
+    <link rel="stylesheet" href="assets/css/header-style.css">
+    <link rel="stylesheet" href="assets/css/welcome-style.css">
+    <script src="https://kit.fontawesome.com/3198643e95.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
-    <p>Salut à toi, <span style="color: blue"><?= $_COOKIE['pseudo'] ?></span> !</p>
-    <a href="profil.php"></a>
+    <?php require 'header.php'; ?>
+
+    <p>Salut à toi, <span style="color: blue">
+            <?php
+            $pseudo = $_COOKIE['pseudo'];
+            echo $pseudo;
+
+//            if (!isset($pseudo)) {
+//                sendToPageWithError('login.php', 'Votre session a expiré. Veuillez vous reconnecter.');
+//            } else {
+//                echo $pseudo;
+//            }
+            ?>
+        </span> !</p>
+    <script src="assets/js/header-dynamic.js"></script>
 </body>
 </html>
