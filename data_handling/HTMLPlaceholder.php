e8faff4ca2907faa825504409b0f39e60c0d7436
tarturr <tartur.dev@gmail.com>
2023-11-24 09:49:27 +0100

    Added an HTMLPlaceholder class to contain all the HTMLDisplays of the current page.

---
+++ data_handling/HTMLPlaceholder.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'HTMLDisplay.php';
+
+class HTMLPlaceholder {
+    private string $pageName;
+    private array $displays;
+
+    public function __construct($pageName, $displays) {
+        $this->pageName = $pageName . '.php';
+        $this->displays = $displays;
+    }
+
+    public function display($key): void {
+        foreach ($this->displays as $display) {
+            if (strcasecmp($key, $display->getKey()) == 0) {
+                $display->display();
+                return;
+            }
+        }
+
+        throw new InvalidArgumentException('The "' . $key . '" does not exist in the ' . $this->pageName . ' html placeholder.');
+    }
+}
