c8828ac35c6ef1ea49c862a2609c9f8f7273274f
tarturr <tartur.dev@gmail.com>
2024-01-27 09:44:46 +0100

    Temporary index.php file and its resources

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DiscordIn - Accueil</title>
+    <link rel="stylesheet" href="assets/css/header-style.css">
+    <link rel="stylesheet" href="assets/css/index-style.css">
+</head>
+<body>
+    <?php require_once 'header.php' ?>
+
+    <section id="landing-page">
+        <article class="presentation">
+            <h1>Le réseau social des jeunes et étudiants.</h1>
+            <p>
+                Créé en 2023, DiscordIn est un réseau social utilisable par tous les lycéens souhaitant communiquer
+                entre eux et avoir un profil professionnel !
+            </p>
+
+            <a href="" class="bold btn">adazdazdaaaaaaaaaaaaaaaaaaaaa</a>
+        </article>
+
+        <img src="assets/img/jeunes.jpg" alt="Photo de jeunes sur leur écran.">
+    </section>
+</body>
+</html>
