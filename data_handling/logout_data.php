0c5c4f03b35da2901914f8900e28889251d23e3a
tarturr <tartur.dev@gmail.com>
2024-03-15 08:44:22 +0100

    Deleted all unused folders & files.

--- data_handling/logout_data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Supprimer le cookie pour toutes les pages (mettre une durée de time() signifie que la durée du cookie est à 0 seconde).
-setcookie('pseudo', '', time(), '/');
-header('Location: ../' . $_GET['redirectTo']);
-exit();
