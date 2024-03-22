049ba1bbc2c429a3f4b3b6ce0dc52787c3ee2ddd
tarturr <tartur.dev@gmail.com>
2024-03-22 08:31:16 +0100

    Defining single-line instructions so that the MySQL module executes instructions line by line.

--- pandamonium/schema.sql
+++ pandamonium/schema.sql
@@ -14,16 +14,7 @@
 --
 
 DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `pseudo` varchar(16) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `mot_de_passe` varchar(64) NOT NULL,
-  `date_naiss` date NOT NULL,
-  `date_enregistre` date NOT NULL,
-  `date_connecte` date NOT NULL,
-  `liste_amis` text,
-  PRIMARY KEY (`pseudo`)
-);
+CREATE TABLE IF NOT EXISTS `utilisateur` (`pseudo` varchar(16) NOT NULL, `email` varchar(50) NOT NULL, `mot_de_passe` varchar(64) NOT NULL, `date_naiss` date NOT NULL, `date_enregistre` date NOT NULL, `date_connecte` date NOT NULL, `liste_amis` text, PRIMARY KEY (`pseudo`));
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
