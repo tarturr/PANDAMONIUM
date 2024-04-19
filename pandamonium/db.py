f210c08fe07598a91d11348e8a35352fab5ea725
tarturr <tartur.dev@gmail.com>
2024-04-19 08:54:27 +0200

    Moved base_column module content to db module.

--- pandamonium/db.py
+++ pandamonium/db.py
@@ -1,7 +1,27 @@
+import functools
+
 import flask as fk
 
 import mysql.connector as connector
 import mysql.connector.abstracts as abstracts
+
+from pandamonium.security import set_security_error
+
+
+def requires(func):
+    @functools.wraps(func)
+    def wrapper(**kwargs):
+        for key in kwargs:
+            value = kwargs[key]
+            error_msg = func(**kwargs)
+
+            if error_msg:
+                set_security_error(error_msg)
+                return False
+
+        return True
+
+    return wrapper
 
 
 def get_db() -> abstracts.MySQLConnectionAbstract:
