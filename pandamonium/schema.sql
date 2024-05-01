b253e3ce789f513d5d9ba7953109e1d1e720cba4
tarturr <tartur.dev@gmail.com>
2024-05-01 17:49:58 +0200

    Changed user table structure.

--- pandamonium/schema.sql
+++ pandamonium/schema.sql
@@ -14,7 +14,7 @@
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (`username` varchar(16) NOT NULL, `email` varchar(50) NOT NULL, `password` varchar(64) NOT NULL, `date_of_birth` date NOT NULL, `registered_at` date NOT NULL, `logged_at` date NOT NULL, `friends` text, PRIMARY KEY (`username`));
+CREATE TABLE users(uuid VARCHAR(36), username VARCHAR(50) NOT NULL, email VARCHAR(50), password VARCHAR(50), date_of_birth DATE, friends VARCHAR(3600), relations VARCHAR(3600), registration_date DATE, last_connection_date DATETIME, pronouns VARCHAR(50), pb_displayed_name VARCHAR(50), pb_bio VARCHAR(300), pv_displayed_name VARCHAR(50), pv_bio VARCHAR(300), PRIMARY KEY(uuid));
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
