c2365da95fffc3b012b0317d2d75298e6326d06c
Gaëtan <145026536+gae35@users.noreply.github.com>
2024-05-03 17:29:19 +0200

    Completed the bamboo class (without extending it from Entity because it was sort of useless)

--- pandamonium/bamboo.py
+++ pandamonium/bamboo.py
@@ -10,6 +10,7 @@ from pandamonium.security import check_password, date_from_string, date_to_strin
 
 from uuid import uuid4
 
+
 class Bamboo:
     """Classe représentant un serveur unique du réseau social.
     Par "bambou", nous parlons d'un endroit virtuel créé sur notre réseau social pour discuter de façon communautaire.
@@ -18,50 +19,47 @@ class Bamboo:
 
     def __init__(
             self,
-            server_id: str,
+            bamboo_uuid: str,
             name: str,
             creator: str,
             creation_time: str,
             pic: str,
             banner: str
     ):
-        self.id = server_id
-        self.name = name
-        self.creator = creator
-        self.creation_time = creation_time
-        self.pic = pic
-        self.banner = banner
+        self.uuid = bamboo_uuid
+
+        db = get_db()
+        with db.cursor() as curs:
+            curs.execute(
+                'SELECT name, creation_time, uuid_1 FROM bamboos WHERE uuid = %',
+                self.uuid
+            )
+            bamboo = curs.fetchone()
+            self.name = bamboo[0]
+            self.creation_time = bamboo[1]
+            self.creator = bamboo[2]
 
     def create(self, name):
-        self.id = uuid4()
+        self.uuid = uuid4()
         self.name = name
         self.creator = fk.session['username']
         self.creation_time = datetime.now()
-        self.pic = None
-        self.banner = None
 
         db = get_db()
         db.cursor().execute(
             'INSERT INTO bamboos(uuid, name, creation_time, creator) VALUES (%s, %s, %s, %s)',
-            (self.id, self.name, self.creation_time, self.creator)
+            (self.uuid, self.name, self.creation_time, self.creator)
         )
 
     def update(
             self,
             name: str,
-            pic: str,
-            banner: str
     ):
         if self.name != name:
             self.name = name
-        if self.pic != pic:
-            self.pic = pic
-        if self.banner != banner:
-            self.banner = banner
 
         db = get_db()
-
         db.cursor().execute(
             'UPDATE servers SET name = %s, pic = %s, banner = %s WHERE id = %s',
-            (self.name, self.pic, self.banner, self.id)
+            (self.name, self.pic, self.banner, self.uuid)
         )
