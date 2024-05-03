dd17f35f5a38f15cd6a471ca3f492c6c241c67f1
Gaëtan <145026536+gae35@users.noreply.github.com>
2024-05-03 18:32:48 +0200

    Cleant the file (added docstrings & removed useless functions) Added the 'members' attribute to the Bamboo class To have attributes of the right type, made use of the following functions from pandamonium.security : date_from_string, date_to_string, uuid_split

--- pandamonium/bamboo.py
+++ pandamonium/bamboo.py
@@ -2,11 +2,8 @@ import flask as fk
 
 from datetime import datetime
 
-import mysql.connector.cursor as cursor
-
 from pandamonium.database import get_db
-from pandamonium.security import check_password, date_from_string, date_to_string, fill_requirements, \
-    set_security_error
+from pandamonium.security import date_from_string, date_to_string, uuid_split
 
 from uuid import uuid4
 
@@ -19,47 +16,51 @@ class Bamboo:
 
     def __init__(
             self,
-            bamboo_uuid: str,
-            name: str,
-            creator: str,
-            creation_time: str,
-            pic: str,
-            banner: str
+            bamboo_uuid: str
     ):
+        """Ctor d'un bambou. Prend en paramètre l'uuid du bamboo et récupère dans la base de données
+        les différents attributs de l'instance : nom, date de création, uuid du créateur et membres (sous la forme d'une
+        liste)."""
+
         self.uuid = bamboo_uuid
 
         db = get_db()
         with db.cursor() as curs:
             curs.execute(
-                'SELECT name, creation_time, uuid_1 FROM bamboos WHERE uuid = %',
+                'SELECT name, creation_time, uuid_1, members FROM bamboos WHERE uuid = %s',
                 self.uuid
             )
             bamboo = curs.fetchone()
             self.name = bamboo[0]
-            self.creation_time = bamboo[1]
+            self.creation_time = date_from_string(bamboo[1])
             self.creator = bamboo[2]
+            self.members = uuid_split(bamboo[3])
 
     def create(self, name):
+        """Méthode qui ajoute un bambou à la base de données et instancie le bambou dans Python."""
         self.uuid = uuid4()
         self.name = name
         self.creator = fk.session['username']
-        self.creation_time = datetime.now()
+        self.creation_time = date_to_string(datetime.now())
 
         db = get_db()
         db.cursor().execute(
-            'INSERT INTO bamboos(uuid, name, creation_time, creator) VALUES (%s, %s, %s, %s)',
-            (self.uuid, self.name, self.creation_time, self.creator)
+            'INSERT INTO bamboos(uuid, name, creation_time, creator, members) VALUES (%s, %s, %s, %s, %s)',
+            (self.uuid, self.name, self.creation_time, self.creator, self.creator)
         )
 
     def update(
             self,
             name: str,
     ):
+
+        """Méthode permettant de modifier les informations """
+
         if self.name != name:
             self.name = name
 
-        db = get_db()
-        db.cursor().execute(
-            'UPDATE servers SET name = %s, pic = %s, banner = %s WHERE id = %s',
-            (self.name, self.pic, self.banner, self.uuid)
-        )
+            db = get_db()
+            db.cursor().execute(
+                'UPDATE servers SET name = %s WHERE id = %s',
+                (self.name, self.uuid)
+            )
