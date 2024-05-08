d8d3272244e7967f654c58c81326b87e377a1acf
tarturr <tartur.dev@gmail.com>
2024-05-08 15:45:17 +0200

    Ignored category table. for now.

--- pandamonium/schema.sql
+++ pandamonium/schema.sql
@@ -20,8 +20,8 @@ DROP TABLE IF EXISTS branches;
 DROP TABLE IF EXISTS messages;
 CREATE TABLE users(uuid VARCHAR(36), username VARCHAR(50) NOT NULL, email VARCHAR(50), password VARCHAR(512), date_of_birth DATE, friends VARCHAR(3600), relations VARCHAR(3600), bamboos VARCHAR(3600), registration_date DATE, last_connection_date DATETIME, pronouns VARCHAR(50), public_display_name VARCHAR(50), public_bio VARCHAR(300), private_display_name VARCHAR(50), private_bio VARCHAR(300), PRIMARY KEY(uuid), UNIQUE(username), UNIQUE(email));
 CREATE TABLE bamboos(uuid VARCHAR(36), name VARCHAR(50) NOT NULL, creation_date DATE, members TEXT, owner_uuid VARCHAR(36) NOT NULL, PRIMARY KEY(uuid), FOREIGN KEY(owner_uuid) REFERENCES users(uuid));
-CREATE TABLE category(uuid VARCHAR(36), name VARCHAR(20), bamboo_uuid VARCHAR(36) NOT NULL, PRIMARY KEY(uuid), FOREIGN KEY(bamboo_uuid) REFERENCES bamboos(uuid));
-CREATE TABLE branches(uuid VARCHAR(36), name VARCHAR(30) NOT NULL, category_uuid VARCHAR(36) NOT NULL, PRIMARY KEY(uuid), FOREIGN KEY(category_uuid) REFERENCES category(uuid));
+/*CREATE TABLE category(uuid VARCHAR(36), name VARCHAR(20), bamboo_uuid VARCHAR(36) NOT NULL, PRIMARY KEY(uuid), FOREIGN KEY(bamboo_uuid) REFERENCES bamboos(uuid));*/
+CREATE TABLE branches(uuid VARCHAR(36), name VARCHAR(30) NOT NULL, bamboo_uuid VARCHAR(36) NOT NULL, PRIMARY KEY(uuid), FOREIGN KEY(bamboo_uuid) REFERENCES bamboos(uuid));
 CREATE TABLE messages(uuid VARCHAR(36), content VARCHAR(2000) NOT NULL, date_sent DATETIME, modified BOOLEAN NOT NULL, sender_uuid VARCHAR(36) NOT NULL, branch_uuid VARCHAR(36) NOT NULL, response_to_message_uuid VARCHAR(36), PRIMARY KEY(uuid), FOREIGN KEY(sender_uuid) REFERENCES users(uuid), FOREIGN KEY(branch_uuid) REFERENCES branches(uuid), FOREIGN KEY(response_to_message_uuid) REFERENCES messages(uuid));
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
